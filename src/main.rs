7765201a1b6f80e49a620d677e95c1b38b3ab4ee
maksymalist <maksymvovkk@gmail.com>
2023-09-12 20:20:53 -0400

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use anyhow::{Context, Result};
+use std::process::Command;
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+    let output = Command::new(command)
         .args(command_args)
         .output()
         .with_context(|| {
