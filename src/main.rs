304ddd56b5b1aecd092b7d753d2a86485c077287
maksymalist <maksymvovkk@gmail.com>
2023-09-12 20:33:12 -0400

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use anyhow::{Context, Result};
-use std::process::Command;
+use std::io::Write;
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+
     let args: Vec<_> = std::env::args().collect();
+
+    // path of the docker executable
     let command = &args[3];
+
+    // arguments to the docker executable
     let command_args = &args[4..];
-    let output = Command::new(command)
+
+    // run the docker command
+    let output = std::process::Command::new(command)
         .args(command_args)
         .output()
         .with_context(|| {
@@ -16,9 +22,10 @@ fn main() -> Result<()> {
             )
         })?;
 
+    // print the output of the docker command
     if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out.replace("\n", ""));
+        std::io::stdout().write_all(&output.stdout)?;
+        std::io::stderr().write_all(&output.stderr)?;
     } else {
         std::process::exit(1);
     }
