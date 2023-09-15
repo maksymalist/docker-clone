5435c2af04743c3b1bf5149d674f49ceeddbfdbd
maksymalist <maksymvovkk@gmail.com>
2023-09-15 07:26:47 -0400

    pass 3rd stage?

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,42 @@
 extern crate libc;
-
-use std::ffi::CString;
-use libc::{c_int, chroot};
-
-use anyhow::Result;
-use std::io::Write;
-
-use std::string::String;
-
+use anyhow::{Context, Result};
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 
-
-pub fn chroot_directory(path: &str) -> Result<(), String> {
-    let path_c = CString::new(path).map_err(|_| "CString conversion failed")?;
-
-    let result = unsafe {
-        chroot(path_c.as_ptr())
-    };
-
-    if result == 0 {
-        Ok(())
-    } else {
-        Err("chroot failed".to_string())
-    }
-}
-
 fn main() -> Result<()> {
-
     let args: Vec<_> = std::env::args().collect();
-    let path = "./chroot";
-    
-    chroot_directory(path).ok();
-
-    // path of the docker executable
     let command = &args[3];
-
-    // arguments to the docker executable
     let command_args = &args[4..];
-
-    // run the docker command
+    // Create a temproty directory
+    let temp_dir = tempfile::tempdir()?;
+    let dir_path = temp_dir.path().to_path_buf();
+    // Copy command to the temporary directory
+    let dest_path = temp_dir.path().join(command.strip_prefix("/").unwrap());
+    if let Some(parent) = dest_path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+    std::fs::copy(command, dest_path)?;
+    let c_dir_path = std::ffi::CString::new(dir_path.to_str().unwrap()).unwrap();
+    // chroot into the temporary directory
+    unsafe {
+        libc::chroot(c_dir_path.as_ptr());
+    }
+    // Create empty /dev/null
+    std::fs::create_dir_all("/dev")?;
+    std::fs::File::create("/dev/null")?;
     let output = std::process::Command::new(command)
         .args(command_args)
-        .output()?;
+        .output()
+        .with_context(|| {
+            format!(
+                "Tried to run '{}' with arguments {:?}",
+                command, command_args
+            )
+        })?;
 
-    // print the output of the docker command
-    std::io::stdout().write_all(&output.stdout)?;
-    std::io::stderr().write_all(&output.stderr)?;
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    print!("{}", stdout);
+    eprint!("{}", stderr);
 
-    // exit with the same exit code as the docker command
     std::process::exit(output.status.code().unwrap_or(1));
 }
